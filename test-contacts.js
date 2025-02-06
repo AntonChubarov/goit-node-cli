5b52e0b3d9b9f88fc1bdce156bd768739e47ee65
AntonChubarov <achlp600@gmail.com>
2025-02-06 17:53:09 -0500

    test: scenario updated

--- test-contacts.js
+++ test-contacts.js
@@ -4,7 +4,8 @@ const commands = [
     'node src/index.js -a list',
     'node src/index.js -a get -i "31abfbf7-b0e9-4d3e-9187-bc46ea9caa28"',
     'node src/index.js -a add -n "Mango Jango" -e "mango@gmail.com" -p "(571) 322-2222"',
-    'node src/index.js -a remove -i "5802e71e-6975-4200-88f7-d9b937f6a47f"'
+    'node src/index.js -a remove -i "5802e71e-6975-4200-88f7-d9b937f6a47f"',
+    'node src/index.js -a list',
 ];
 
 commands.forEach(cmd => {
